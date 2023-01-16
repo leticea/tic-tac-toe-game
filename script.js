67da772947552a1aa31f5f3de8ded7f0f58b5e46
leticea <nascimento.leticeah@gmail.com>
2023-01-16 18:00:05 -0300

    check winner

--- script.js
+++ script.js
@@ -32,3 +32,18 @@ function play(y) {
   }
 }
 
+function checkWinner(a, b, c) {
+  a--;
+  b--;
+  c--;
+  if ((span[a].dataset.player === span[b].dataset.player)
+    && (span[b].dataset.player === span[c].dataset.player)
+    && (span[a].dataset.player === span[c].dataset.player)
+    && (span[a].dataset.player === "x" || span[a].dataset.player === "o")
+    && isGameOver == false) {
+      span[a].parentNode.className += " activeBox";
+      span[b].parentNode.className += " activeBox";
+      span[c].parentNode.className += " activeBox";
+      isGameOver(a);
+    }
+}
